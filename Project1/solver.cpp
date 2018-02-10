3ddbc15521ce76c680044e17addfc92b2d47a62e
hannahberg <hannah.brg@gmail.com>
2018-02-10 16:44:27 +0100

    fixed syntax errors

--- Project1/solver.cpp
+++ Project1/solver.cpp
@@ -4,7 +4,7 @@
 using namespace arma;
 
 
-void Solver::solve(mc=10,N=1){
+void Solver::solve(int mc, int N){// need mc, N
 
     random_device rd;
     mt19937 gen(rd());
@@ -13,7 +13,12 @@ void Solver::solve(mc=10,N=1){
     // loop over alpha
     int num_alpha = 0;
     int dim = 1;
-    while(num_alpha < len(alpha)){
+    vec alpha;
+    double pdf;
+    double current_alpha;
+    //nt len_alpha = size(alpha);
+    while(num_alpha < sizeof(alpha)){
+        current_alpha = alpha(num_alpha);
         // initialize random positions
         int i; int j;
         //i = rand() % N;
@@ -30,7 +35,7 @@ void Solver::solve(mc=10,N=1){
         // iterate over MC cycles
         for(i=0;i<mc;i++){
            //set up PDF |phi|^2
-           pdf = PDF(R);
+           pdf = PDF(R,current_alpha);
            vec Rnew;
 
            //propose a new position Rnew(boson_j) by moving one boson from position R(boson_j) one at the time
@@ -38,9 +43,9 @@ void Solver::solve(mc=10,N=1){
                 Rnew = zeros(N); // to suggest new position for boson
                 r = doubleRNG(gen) - 0.5;
                 Rnew(j) = R(j) + r*rho;
-                pdf = PDF(R);
+                pdf = PDF(R,current_alpha); // now we calculate it two times... not necessary
                 // how to determine if we accept or reject new position
-                A = (wavefunc(Rnew,alpha))/wavefunc(R,alpha);
+                A = (wavefunc(Rnew,current_alpha))/wavefunc(R,current_alpha);
                 if(A > 1){
                     //accept new position
                     R(j) = Rnew(j);
@@ -57,21 +62,21 @@ void Solver::solve(mc=10,N=1){
 
            }
         }
-        num_alpha += 1
+        num_alpha += 1;
     }
 }
 
-void wavefunc(R,alpha){
+double Solver::wavefunc(vec R, double alpha_){// need R, alpha
     //bool interact = y/n
     //g = exp(-alpha*(x(i)^2 + y(i)^2 + beta*z(i)^2));
-    g = exp(-alpha*dot(R,R));
-    f = 1; //no interaction here!!
-    phi = g*f;
+    g = exp(-alpha_*dot(R,R));
+    f = ones(1); //no interaction here!!
+    phi = dot(g,f);
     return phi;
 }
 
-void PDF(vec R){
-    return abs(wavefunc(R))^2;
+double Solver::PDF(vec R, double alpha_){
+    return pow(abs(wavefunc(R, alpha_)),2);
 }
 
 Solver::Solver(double beta, double hbar, double m, double omega, double a_h0, double alpha, double rho){
@@ -80,7 +85,7 @@ Solver::Solver(double beta, double hbar, double m, double omega, double a_h0, do
     m = 1;
     omega = 1;
     a_h0 = sqrt(hbar/(m*omega));
-    alpha = 1./(2*a_h0^2);
+    alpha = 1./((2*a_h0)*(2*a_h0));
     //W = zeros(N,N)
     rho = 0.001;
 }
