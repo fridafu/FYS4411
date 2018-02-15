5ede6696e149bdccd92ee3f313eb22dc2a404395
hannahberg <hannah.brg@gmail.com>
2018-02-15 12:40:23 +0100

    cleaned up the code

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,22 +7,18 @@ using namespace arma;
 
 
 int main(){
-    //vec R = zeros(10,2);
-    //cout << R << endl;
-    // return 0;
     double a_h0 = 1.;
     double alpha = 1./((2*a_h0)*(2*a_h0));
-    //W = zeros(N,N)
     double rho = 0.001;
+    // monte carlo cycles
     int mc = 10;
-    int numpart=10;
+    // N particles
+    int numpart = 10;
     int howmanyDs = 3;
-
-    Solver S(1, 1, 1, 1, 1, 0.001, 10, numpart, howmanyDs);
+    // initialize Solver class
+    Solver S(1, 1, 1, 1, 1, 0.001, mc, numpart, howmanyDs);
     // Solver(double s_beta, double s_hbar, double mass, double s_omega, double s_alpha, double s_rho, int s_mc, int s_N, int s_dim)
-    //int N = 1;
-    //vec alphatest = ones(10);
-    //cout<<size(alphatest,0) << endl;
+
     for (int i = 0; i < 100; i++){
         S.solve();
     }
