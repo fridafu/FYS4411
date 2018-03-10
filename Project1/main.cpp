81ebfca4bf4d0c6748520c0b363b354b8087f746
fridafu <31306289+fridafu@users.noreply.github.com>
2018-03-10 17:08:35 +0100

    Herrefunkeittj

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,13 +8,9 @@ using namespace arma;
 
 int main(){
     double alpha = 0.5;
-
     double rho = 0.1;
     double dt = 0.001;
-
-    // monte carlo cycles
-    int mc = 1000000;
-    // N particles
+    int mc = 100000; // monte carlo cycles
     int numpart = 100; //CHANGE THE NAME!!!!!!!!!!!!!!!!!!!!!!!!!
     int howmanyDs = 3;
     double beta = 1;
@@ -22,16 +18,13 @@ int main(){
     double mass = 1;
     double omega = 1;
     double h = 1e-8;
-    // initialize Solver class
-    Solver S(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt);
-    //Solver(double s_beta, double s_hbar, double mass, double s_omega, double s_alpha, double s_rho, int s_mc, int s_N, int s_dim, double s_h);
+
+    Solver S(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt); // initialize Solver class
     ofstream myfile;
-
     /*CHANGE MY NAME!!!!!!!!!!!!!  DONT YOU DARE NOT CHANGE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
-    myfile.open("balle.dat");
+    myfile.open("1644.dat");
     //S.solve(myfile);
-    //S.solve_num(myfile);
-    S.langevin(myfile);
+    S.solve_num(myfile);
+    //S.langevin(myfile);
     myfile.close();
-
 }
