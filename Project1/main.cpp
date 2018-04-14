6d2cb2bb787c4f6aa9f53fa4bd4f5641fb8c5f08
hannahberg <hannah.brg@gmail.com>
2018-04-14 21:05:45 +0200

    blocking is now in a notebook

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,14 +3,14 @@
 #include "bruteforce.h"
 #include "impsamp.h"
 #include "interact.h"
-#include "catch.hpp"
+//#include "catch.hpp"
 
 using namespace std;
 using namespace arma;
 
 int main(){
     vec alphavec = linspace<vec>(0.3, 0.7, 9);
-    vec dtvec = logspace<vec>(-4,0,51);
+    vec dtvec = logspace<vec>(-7,1,17);
     cout << dtvec << endl;
     double rho = 0.1;
     //double dt = 0.01; // [0.001, 0.01]
@@ -24,39 +24,43 @@ int main(){
     double omega = 1;
 
     ofstream myfile;
-    myfile.open("dt_table_test.dat");
-    //for(int elem=0; elem<size(dtvec,0); elem++){
-    double alpha = 0.5; //alphavec(elem);
-    double dt = 0.0000001;//dtvec(elem);
-    Solver S(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt); // initialize Solver class
-    Bruteforce* B = new Bruteforce(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt);
-    Impsamp* Imp = new Impsamp(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt);
-    Interact* Int = new Interact(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt);
 
-    ofstream myfile2;
-    ofstream myfile3;
-    ofstream myfile4;
-    ofstream myfile5;
-    string filename ="c_n" + std::to_string(numpart) + "_d" + std::to_string(howmanyDs) + "_mc" + std::to_string(mc);
-    //myfile.open(filename + "dt" + std::to_string(dt) + ".dat");     //CHANGE MY NAME!!!!!!!!!!!!!  DONT YOU DARE NOT CHANGE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //myfile2.open("Ea_" + filename + "_alpha"  + std::to_string(alpha) + ".dat");
-    //myfile3.open("En_" + filename + "_alpha" + std::to_string(alpha) + ".dat");
-    //myfile4.open("Eimp_" + filename + "dt" + std::to_string(dt) + ".dat");
-    myfile4.open(std::to_string(dt) + ".dat");
-    //myfile5.open("Eint_" + filename + "_alpha" + std::to_string(alpha) + ".dat");
-    //B->solve(myfile,myfile2);
-    //B->solve_num(myfile,myfile3);
-    Imp->langevin(myfile,myfile4,alpha);
-    //Int->solve_interact(myfile, myfile5, alpha);
-    //myfile.close();
-    //myfile2.close();
-    //myfile3.close();
-    myfile4.close();
-    //myfile5.close();
-    //Imp->best_alpha();
-    delete Int;
-    delete Imp;
-    delete B;
-    //}
+    string folder = "alpha0_45/";
+    myfile.open(folder + "dt_table_test.dat");
+
+    for(int elem=0; elem<size(dtvec,0); elem++){
+        double alpha = 0.45; //alphavec(elem);
+        double dt = dtvec(elem); // 0.0000001;//
+        cout << "Doing " << scientific << to_string(dt) << endl;
+        Solver S(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt); // initialize Solver class
+        Bruteforce* B = new Bruteforce(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt);
+        Impsamp* Imp = new Impsamp(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt);
+        Interact* Int = new Interact(beta, hbar, mass, omega, alpha, rho, mc, numpart, howmanyDs, h, dt);
+
+        ofstream myfile2;
+        ofstream myfile3;
+        ofstream myfile4;
+        ofstream myfile5;
+        string filename =folder + "c_n" + std::to_string(numpart) + "_d" + std::to_string(howmanyDs) + "_mc" + std::to_string(mc);
+        //myfile.open(filename + "dt" + std::to_string(dt) + ".dat");     //CHANGE MY NAME!!!!!!!!!!!!!  DONT YOU DARE NOT CHANGE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //myfile2.open("Ea_" + filename + "_alpha"  + std::to_string(alpha) + ".dat");
+        //myfile3.open("En_" + filename + "_alpha" + std::to_string(alpha) + ".dat");
+        //myfile4.open("Eimp_" + filename + "dt" + std::to_string(dt) + ".dat");
+        myfile4.open(folder+std::to_string(log10(dt)) + ".dat");
+        //myfile5.open("Eint_" + filename + "_alpha" + std::to_string(alpha) + ".dat");
+        //B->solve(myfile,myfile2);
+        //B->solve_num(myfile,myfile3);
+        Imp->langevin(myfile,myfile4,alpha);
+        //Int->solve_interact(myfile, myfile5, alpha);
+        //myfile.close();
+        //myfile2.close();
+        //myfile3.close();
+        myfile4.close();
+        //myfile5.close();
+        //Imp->best_alpha();
+        delete Int;
+        delete Imp;
+        delete B;
+    }
     myfile.close();
 }
